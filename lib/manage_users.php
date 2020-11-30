7002336b95ff6a9215efeea5d4cf0cf0901780df
alexc <alexcolit@hotmail.fr>
2020-11-30 16:33:37 -0500

    ui updated

--- lib/manage_users.php
+++ lib/manage_users.php
@@ -11,194 +11,162 @@ session_start();
 </head>
 
 
-<body>
+<body style="background-color: #d5e2ff;">
+<div style = "background-color:#aca3ec; height:100px; color:#4D39D6; padding:3px;"><b><br><i class="huge chess rook icon"></i></b><b align="middle" style="margin-bottom:10px; color:white; font-size:40px;">CON</b></div>
+    <br>
+    <br>
+    <a style="margin:30px; font-size: 40px; color:black;" class="item">
+        Manage Users  <i class="wrench icon"></i>
+    </a>
 
-Create user: 
-<br>
-<br>
+    <table class="ui selectable inverted table">
+        <thead>
+        <tr>
+            <th></th>
+            <th>Name</th>
+            <th> password </th>
+            <th> email </th>
+            <th> address </th>
+            <th> admin </th>
+            <th> status </th>
+            <th> condoClass </th>
+        </tr>
 
-<?php
+        </thead>
+        <?php
 
-if(isset($_POST['create_user'])) {	//handle create user button press
-	
-	//validate form
-	$valid = true;
-	if( !isset($_POST['name']) || empty($_POST['name']) ) $valid = false;
-	if(!$valid) echo "name";
-	if( !isset($_POST['password']) || empty($_POST['password']) ) $valid = false;
-	if(!$valid) echo "pass";
-	if( !isset($_POST['email']) || empty($_POST['email']) ) $valid = false;
-	if(!$valid) echo "email";
-	if( !isset($_POST['address']) || empty($_POST['address']) ) $valid = false;
-	if(!$valid) echo "address";
-	if( !isset($_POST['status']) || empty($_POST['status']) ) $valid = false;
-	if(!$valid) echo "status";
-	if( !isset($_POST['condoClass']) || empty($_POST['condoClass']) ) $valid = false;
-	if(!$valid) echo "condoClass";
-	
-	if($valid) {
-		
-		$name = $_POST['name']; $password = $_POST['password'];
-		$email = $_POST['email']; $address = $_POST['address'];
-		$status = $_POST['status']; $condoClass = $_POST['condoClass'];
-		
-		if(isset($_POST['admin']) && $_POST['admin'] == 'on') $admin=1;
-		else $admin=0;
-		
-		//admins for this condo association can only add users to this condo association
-		$condoAssociationID = $_SESSION['condoAssociationID'];
-		
-		
-		$sql = "INSERT INTO Users (name, password, email, primary_address, administrator, status, condoAssociationID, condoClassification) VALUES ('$name', '$password', '$email', '$address', $admin, '$status', $condoAssociationID, '$condoClass')";
-		if (mysqli_query($db, $sql)) echo "new user created"."<br><br>";
-		else echo "failed to create new user<br><br>";
-		
-		//this is to prevent more accounts being made on page
-		//refresh after successfully creating an account
-		//works because it clears $_post
-		header("Location: " . $_SERVER['PHP_SELF']);
-	
-	} else echo "please fill out all input fields to create a user<br><br>";
-	
-}
-?>
-<div style = "background-color:#aca3ec; color:#4D39D6; padding:3px;"><b>Create User</b></div>
-<br>
-<form action="../admin_page.html" method="post">
-    <button class="ui blue left labeled icon button" type="submit" name="back" >
-        <i class="left arrow icon"></i>
-        Back to admin page
-    </button>
-</form>
+        $user_query = mysqli_query($db, "SELECT * FROM Users");
+        while($user = mysqli_fetch_array($user_query)){
+            echo "<tr>";
+            echo '<td><form action="edit_user.php" method="post">';
+            echo '<input type="hidden" id="editNum" name="editNum" value="'.$user['userID'].'">';
+            echo '<input class="ui button" type="submit" name="edit" value="edit">';
+            echo "</form></td>";
+            echo "<td>".$user['name']."</td>";
+            echo "<td>".$user['password']."</td>";
+            echo "<td>".$user['email']."</td>";
+            echo "<td>".$user['primary_address']."</td>";
+            echo "<td>".$user['administrator']."</td>";
+            echo "<td>".$user['status']."</td>";
+            echo "<td>".$user['condoClassification']."</td>";
+            echo "</tr>";
+        }
 
+        ?>
+    </table>
+
+    <br>
+    <a style="margin:30px; font-size: 40px; color:black;" class="item">
+        Create Profile  <i class="user icon"></i>
+    </a>
+    <?php
+
+    if(isset($_POST['create_user'])) {	//handle create user button press
+
+        //validate form
+        $valid = true;
+        if( !isset($_POST['name']) || empty($_POST['name']) ) $valid = false;
+        if(!$valid) echo "name";
+        if( !isset($_POST['password']) || empty($_POST['password']) ) $valid = false;
+        if(!$valid) echo "pass";
+        if( !isset($_POST['email']) || empty($_POST['email']) ) $valid = false;
+        if(!$valid) echo "email";
+        if( !isset($_POST['address']) || empty($_POST['address']) ) $valid = false;
+        if(!$valid) echo "address";
+        if( !isset($_POST['status']) || empty($_POST['status']) ) $valid = false;
+        if(!$valid) echo "status";
+        if( !isset($_POST['condoClass']) || empty($_POST['condoClass']) ) $valid = false;
+        if(!$valid) echo "condoClass";
+
+        if($valid) {
+
+            $name = $_POST['name']; $password = $_POST['password'];
+            $email = $_POST['email']; $address = $_POST['address'];
+            $status = $_POST['status']; $condoClass = $_POST['condoClass'];
+
+            if(isset($_POST['admin']) && $_POST['admin'] == 'on') $admin=1;
+            else $admin=0;
+
+            //admins for this condo association can only add users to this condo association
+            $condoAssociationID = $_SESSION['condoAssociationID'];
+
+
+            $sql = "INSERT INTO Users (name, password, email, primary_address, administrator, status, condoAssociationID, condoClassification) VALUES ('$name', '$password', '$email', '$address', $admin, '$status', $condoAssociationID, '$condoClass')";
+            if (mysqli_query($db, $sql)) echo "new user created"."<br><br>";
+            else echo "failed to create new user<br><br>";
+
+            //this is to prevent more accounts being made on page
+            //refresh after successfully creating an account
+            //works because it clears $_post
+            header("Location: " . $_SERVER['PHP_SELF']);
+
+        } else echo "please fill out all input fields to create a user<br><br>";
+
+    }
+    ?>
 <form action="manage_users.php" method="post">
-    <div class="ui two column middle aligned relaxed grid basic segment">
+    <div style="margin:40px;" class="ui two column middle aligned relaxed grid basic segment">
         <div class="column">
-            <div class="ui form segment AVAST_PAM_loginform">
+            <div style=" background-color: #c9d3d8;" class="ui form segment AVAST_PAM_loginform">
                 <div class="field">
                     <label>Name</label>
                     <div class="ui left labeled icon input">
-                        <input type="text" placeholder="Name" name = "name">
+                        <input style=" border: solid;" type="text" placeholder="Name" name = "name">
                         <i class="user icon"></i>
                     </div>
                 </div>
                 <div class="field">
                     <label>Password</label>
                     <div class="ui left labeled icon input">
-                        <input type="password" name = "password">
+                        <input style=" border: solid;" type="password" name = "password">
                         <i class="lock icon"></i>
                     </div>
                 </div>
                 <div class="field">
                     <label>Email</label>
                     <div class="ui left labeled icon input">
-                        <input type="text" placeholder="email" name = "email">
+                        <input style=" border: solid;" type="text" placeholder="email" name = "email">
                         <i class="at icon"></i>
                     </div>
                 </div>
                 <div class="field">
                     <label>Address</label>
                     <div class="ui left labeled icon input">
-                        <input type="text" placeholder="Address" name = "address">
+                        <input style=" border: solid;" type="text" placeholder="Address" name = "address">
                         <i class="building icon"></i>
                     </div>
-                </div>
+                    </div>
                 <div class="field">
                     <label>Status</label>
                     <div class="ui left labeled icon input">
-                        <input type="text" placeholder="Status" name = "status">
+                        <input style=" border: solid;" type="text" placeholder="Status" name = "status">
                     </div>
                 </div>
                 <div class="field">
                     <label>Condo Classification</label>
                     <div class="ui left labeled icon input">
-                        <input type="text" placeholder="condoClassification" name = "condoClass">
+                        <input style=" border: solid;" type="text" placeholder="condoClassification" name = "condoClass">
                         <i class="building outline icon"></i>
                     </div>
                 </div>
                 <div class="ui checkbox">
-                    <label for="admin">Administrator </label>
                     <input type="checkbox" id="admin" name="admin" >
+                    <label for="admin">Administrator </label>
                 </div>
                 <br>
                 <br>
-
                 <input class="ui positive button" type="submit" name="create_user" value="confirm">
             </div>
         </div>
     </div>
-
-    <!--
-		<label for="name">Name:</label>
-		<br>
-		<input type="text" name="name">
-		<br>
-		<label for="password:">password:</label>
-		<br>
-		<input type="text" id="password" name="password">
-		<br>
-		<label for="email">email:</label>
-		<br>
-		<input type="text" id="email" name="email">
-		<br>
-		<label for="address">address:</label>
-		<br>
-		<input type="text" id="address" name="address">
-		<br>
-		<label for="status">status:</label>
-		<br>
-		<input type="text" id="status" name="status">
-		<br>
-		<label for="condoClass">condo Classification:</label>
-		<br>
-		<input type="text" id="condoClass" name="condoClass">
-		<br>
-		<label for="admin">administrator </label>
-		<input type="checkbox" id="admin" name="admin">
-		<br>
-		<br>
-		<input type="submit" name="create_user" value="submit">
-		-->
 </form>
 
-<!--
 <form action="../admin_page.html" method="post">
-    <input class="ui small blue button" type="submit" name="back" value="return to admin page">
+    <button style="margin:30px;" class="ui blue left labeled icon button" type="submit" name="back" >
+        <i class="left arrow icon"></i>
+        Back to Administrator Options
+    </button>
 </form>
--->
-
-<table class="ui selectable inverted table">
-    <thead>
-    <tr>
-        <th></th>
-        <th>Name</th>
-        <th> password </th>
-        <th> email </th>
-        <th> address </th>
-        <th> admin </th>
-        <th> status </th>
-        <th> condoClass </th>
-	
-<?php
-
-  $user_query = mysqli_query($db, "SELECT * FROM Users");
-  while($user = mysqli_fetch_array($user_query)){
-  		echo "<tr>";
-  		echo '<td><form action="edit_user.php" method="post">';
-  		echo '<input type="hidden" id="editNum" name="editNum" value="'.$user['userID'].'">';
-  		echo '<input type="submit" name="edit" value="edit">';
-  		echo "</form></td>";
-      echo "<td>".$user['name']."</td>";
-      echo "<td>".$user['password']."</td>";
-      echo "<td>".$user['email']."</td>";
-      echo "<td>".$user['primary_address']."</td>";
-      echo "<td>".$user['administrator']."</td>";
-      echo "<td>".$user['status']."</td>";
-      echo "<td>".$user['condoClassification']."</td>";
-      echo "</tr>";
-  }
-	
-?>
 
 </body>
 </html>
