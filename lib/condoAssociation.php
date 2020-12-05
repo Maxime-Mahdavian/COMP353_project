7a9c448b0b67285e4d0d2e65d95a9e6b6d166b55
alexc <alexcolit@hotmail.fr>
2020-12-05 18:44:26 -0500

    ui updated

--- lib/condoAssociation.php
+++ lib/condoAssociation.php
@@ -42,7 +42,7 @@ session_start();
 	//budget field
 	if($_SESSION['admin'] == 1) { //give admins option to edit
 		if(isset($_POST['edit_budget'])) {	//if we're editing, give admins a confirm button
-			echo '	<h2 style="margin-left: 30px"><input type="submit" name="confirm_new_budget" value="confirm">'. "&nbsp;&nbsp;";
+			echo '	<h2 style="margin-left: 30px"><input class="ui green button" type="submit" name="confirm_new_budget" value="confirm">'. "&nbsp;&nbsp;";
 		} else { echo '	<h2 style="margin-left: 30px"><input class="ui black button" type="submit" name="edit_budget" value="edit">'. "&nbsp;&nbsp;"; }	//else show edit button
 	}
 	if(isset($_POST['edit_budget'])) {	//if we're editing, give admins a field
@@ -52,7 +52,7 @@ session_start();
 	//financial status field
 	if($_SESSION['admin'] == 1) {
 		if(isset($_POST['edit_financialStatus'])) {	//if we're editing, give admins a confirm button
-			echo '	<h2 style="margin-left: 30px"><input type="submit" name="confirm_new_financialStatus" value="confirm">'. "&nbsp;&nbsp;";
+			echo '	<h2 style="margin-left: 30px"><input class="ui green button" type="submit" name="confirm_new_financialStatus" value="confirm">'. "&nbsp;&nbsp;";
 		} else { echo '	<h2 style="margin-left: 30px"><input class="ui black button" type="submit" name="edit_financialStatus" value="edit">'. "&nbsp;&nbsp;"; }	//else show edit button
 	}
 	if(isset($_POST['edit_financialStatus'])) {	//if we're editing, give admins a field
